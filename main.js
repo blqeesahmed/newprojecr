3806d93f776c37fff06ae21d24d2478ae6f59eb2
= <=>
2024-08-19 10:48:39 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const inputnumber =document.getElementById("cardnumber")
+inputnumber. addEventListener("input",()=>{let number. inputnumber.value
+    if(umber.selice (0 , 4)=="5100")
+        logo.setAttribute("src", "master.png")
+} else if (number.slice(0, 4) = "4147")
+     {
+logo.setAttribute("src", "visa.png")
+} 
+else {
+logo.setAttribute("src", "logo.png")
+})
